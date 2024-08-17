9e48452b5f87d2960c30b1f22c0f50eb8b6f8995
m4dev <78566289+m4devpl@users.noreply.github.com>
2024-08-17 18:12:25 +0200

    Initial Version

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Open_Sans, Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+export const openSans = Open_Sans({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400']});
